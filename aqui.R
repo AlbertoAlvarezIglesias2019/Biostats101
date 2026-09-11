209b3784f454830bde59daf4e1b1fe008b7947bc
Alvarez-Iglesias <0107579S@universityofgalway.ie>
2026-09-11 14:41:43 +0100

    first commit

---
+++ aqui.R
@@ -0,0 +1,54 @@
+#install.packages('jmvtools', repos='https://repo.jamovi.org')
+# install.packages('jmvtools', repos = c('https://repo.jamovi.org', 'https://cran.r-project.org'))
+
+options(jamovi_home="C:\\Program Files\\jamovi 2.7.38.0")
+
+#jmvtools::addAnalysis(name='scatterplot', title='Scatterplot with Line of Equality')
+
+#jmvtools::addAnalysis(name='ConditionalPlots', title='Conditional Plots')
+
+#jmvtools::addAnalysis(name='basicstatistics', title='Basic Statistics')
+
+#jmvtools::addAnalysis(name='tables', title='Tables')
+
+#jmvtools::addAnalysis(name='onettest', title='One-Sample t..')
+
+#jmvtools::addAnalysis(name='twottest', title='Two-Sample t..')
+
+# jmvtools::addAnalysis(name='oneprop', title='One proportion..')
+
+# jmvtools::addAnalysis(name='twoprop', title='Two proportions..')
+
+# jmvtools::addAnalysis(name='piechart', title='Pie Chart..')
+# jmvtools::addAnalysis(name='barchart', title='Bar Chart..')
+
+#jmvtools::addAnalysis(name='dotplot', title='Dotplot..')
+
+#jmvtools::addAnalysis(name='pairedt', title='Paired t..')
+
+#jmvtools::addAnalysis(name='densityplot', title='Density Plot..')
+
+#jmvtools::addAnalysis(name='onesign', title='One-Sample Sign..')
+
+#jmvtools::addAnalysis(name='mannwhitney', title='Mann-Whitney..')
+
+#jmvtools::addAnalysis(name='anova', title='One-Way ANOVA..')
+
+#jmvtools::addAnalysis(name='chisquare', title='Chi-Square Test for Association..')
+
+#jmvtools::addAnalysis(name='histogram', title='Histogram..')
+
+#jmvtools::addAnalysis(name='power1test', title='1-Sample t..')
+
+#jmvtools::addAnalysis(name='power2test', title='2-Sample t..')
+
+#jmvtools::addAnalysis(name='power2prop', title='2 Proportions..')
+
+#jmvtools::addAnalysis(name='power1prop', title='1 Proportion..')
+
+#jmvtools::create('ConditionalPlots')
+#jmvtools::check(home = "C:/Program Files/jamovi 2.6.44.0")
+#library(jmvcore)
+#library(jmvtools)
+jmvtools::install()
+
